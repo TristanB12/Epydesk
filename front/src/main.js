4ede4b532e7ecf1cf35d301938157fc778d3e35e
TristanB12 <tristan.bourgeois@epitech.eu>
2022-01-15 14:47:33 +0400

    fix: change redirect uri

--- front/src/main.js
+++ front/src/main.js
@@ -8,7 +8,7 @@ Vue.use(msal, {
     auth: {
       clientId: process.env.VUE_APP_CLIENT_ID,
       tenantId: process.env.VUE_APP_TENANT_ID,
-      redirectUri: 'https://ancient-island-68039.herokuapp.com/',
+      redirectUri: 'https://epydesk.herokuapp.com/',
     }
 });
 
