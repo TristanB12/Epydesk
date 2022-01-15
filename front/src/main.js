08b45f78dd04006176bd4c790a2933507eec99d9
TristanB12 <tristan.bourgeois@epitech.eu>
2022-01-15 17:57:09 +0400

    CHORE

--- front/src/main.js
+++ front/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import msal from 'vue-msal'
 
+console.log(process.env);
 Vue.use(msal, {
     auth: {
       clientId: process.env.VUE_APP_CLIENT_ID,
